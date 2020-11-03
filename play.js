6ad293bc94852fcb6d34bd2b66cbfe2e36b41cf4
Rajaa Omary <omary.rajaa@gmail.com>
2020-11-03 18:30:42 +0000

    Stdin Setup Snake game

--- play.js
+++ play.js
@@ -4,6 +4,33 @@ const {connect} = require('./client');
 console.log('Connecting ...');
 connect();
 
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  });
+
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  // \u0003 maps to ctrl+c input
+
+  if (key === '\u0003') {
+    process.exit();
+  }
+  
+};
+
+setupInput();
+
 
 
 
